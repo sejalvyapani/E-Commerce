b9013002ddba663cc53f7eeb88f8959d1350129b
Sejal Vyapani <93273992+sejalvyapani@users.noreply.github.com>
2021-11-18 16:08:34 +0530

    Remove Unnecessary

---
+++ public/dmxAppConnect/config.js
@@ -0,0 +1,83 @@
+dmx.config({
+  "index": {
+    "repeat5": {
+      "meta": [
+        {
+          "name": "id",
+          "type": "text"
+        },
+        {
+          "name": "image",
+          "type": "text"
+        },
+        {
+          "name": "title",
+          "type": "text"
+        }
+      ],
+      "outputType": "array"
+    },
+    "repeat1": {
+      "meta": [
+        {
+          "name": "id",
+          "type": "text"
+        },
+        {
+          "name": "image",
+          "type": "text"
+        },
+        {
+          "name": "title",
+          "type": "text"
+        }
+      ],
+      "outputType": "array"
+    },
+    "repeat6": {
+      "meta": [
+        {
+          "name": "id",
+          "type": "text"
+        },
+        {
+          "name": "image",
+          "type": "text"
+        },
+        {
+          "name": "discount",
+          "type": "text"
+        },
+        {
+          "name": "type",
+          "type": "text"
+        },
+        {
+          "name": "name",
+          "type": "text"
+        },
+        {
+          "name": "final_price",
+          "type": "text"
+        },
+        {
+          "name": "real_price",
+          "type": "text"
+        },
+        {
+          "name": "rating",
+          "type": "number"
+        },
+        {
+          "name": "rating_count",
+          "type": "number"
+        },
+        {
+          "name": "block_type",
+          "type": "text"
+        }
+      ],
+      "outputType": "array"
+    }
+  }
+});
