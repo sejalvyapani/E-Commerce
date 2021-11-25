d5ff433bf1481e03c9ea22a5121c6f9941c444c2
Sejal Vyapani <93273992+sejalvyapani@users.noreply.github.com>
2021-11-25 18:26:02 +0530

    product add in the cart

--- public/dmxAppConnect/config.js
+++ public/dmxAppConnect/config.js
@@ -200,73 +200,16 @@ dmx.config({
         }
       ],
       "outputType": "array"
+    },
+    "repeat5": {
+      "meta": null,
+      "outputType": "array"
     }
   },
   "productdetails": {
     "repeat1": {
-      "meta": [
-        {
-          "name": "product_id",
-          "type": "number"
-        },
-        {
-          "name": "item_id",
-          "type": "number"
-        },
-        {
-          "name": "image",
-          "type": "text"
-        },
-        {
-          "name": "discount",
-          "type": "text"
-        },
-        {
-          "name": "type",
-          "type": "text"
-        },
-        {
-          "name": "name",
-          "type": "text"
-        },
-        {
-          "name": "color",
-          "type": "text"
-        },
-        {
-          "name": "final_price",
-          "type": "text"
-        },
-        {
-          "name": "real_price",
-          "type": "text"
-        },
-        {
-          "name": "rating",
-          "type": "number"
-        },
-        {
-          "name": "rating_count",
-          "type": "number"
-        },
-        {
-          "name": "description",
-          "type": "text"
-        },
-        {
-          "name": "about",
-          "type": "text"
-        },
-        {
-          "name": "product_code",
-          "type": "text"
-        },
-        {
-          "name": "product_materials",
-          "type": "text"
-        }
-      ],
-      "outputType": "array"
+      "meta": null,
+      "outputType": "text"
     },
     "repeat3": {
       "meta": [
@@ -310,17 +253,8 @@ dmx.config({
       "outputType": "array"
     },
     "repeat4": {
-      "meta": [
-        {
-          "name": "id",
-          "type": "number"
-        },
-        {
-          "name": "filter_item",
-          "type": "text"
-        }
-      ],
-      "outputType": "array"
+      "meta": null,
+      "outputType": "text"
     },
     "repeat5": {
       "meta": [
