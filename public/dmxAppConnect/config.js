29ce56a84506eb9aee51b2061dceda9652cf2dcb
Sejal Vyapani <93273992+sejalvyapani@users.noreply.github.com>
2021-11-25 14:33:13 +0530

    work on slider

--- public/dmxAppConnect/config.js
+++ public/dmxAppConnect/config.js
@@ -135,12 +135,138 @@ dmx.config({
         }
       ],
       "outputType": "array"
+    },
+    "ddProductInformation": {
+      "meta": [
+        {
+          "name": "product_id",
+          "type": "number"
+        },
+        {
+          "name": "item_id",
+          "type": "number"
+        },
+        {
+          "name": "image",
+          "type": "text"
+        },
+        {
+          "name": "discount",
+          "type": "text"
+        },
+        {
+          "name": "type",
+          "type": "text"
+        },
+        {
+          "name": "name",
+          "type": "text"
+        },
+        {
+          "name": "color",
+          "type": "text"
+        },
+        {
+          "name": "final_price",
+          "type": "text"
+        },
+        {
+          "name": "real_price",
+          "type": "text"
+        },
+        {
+          "name": "rating",
+          "type": "number"
+        },
+        {
+          "name": "rating_count",
+          "type": "number"
+        },
+        {
+          "name": "description",
+          "type": "text"
+        },
+        {
+          "name": "about",
+          "type": "text"
+        },
+        {
+          "name": "product_code",
+          "type": "text"
+        },
+        {
+          "name": "product_materials",
+          "type": "text"
+        }
+      ],
+      "outputType": "array"
     }
   },
   "productdetails": {
     "repeat1": {
-      "meta": null,
-      "outputType": "text"
+      "meta": [
+        {
+          "name": "product_id",
+          "type": "number"
+        },
+        {
+          "name": "item_id",
+          "type": "number"
+        },
+        {
+          "name": "image",
+          "type": "text"
+        },
+        {
+          "name": "discount",
+          "type": "text"
+        },
+        {
+          "name": "type",
+          "type": "text"
+        },
+        {
+          "name": "name",
+          "type": "text"
+        },
+        {
+          "name": "color",
+          "type": "text"
+        },
+        {
+          "name": "final_price",
+          "type": "text"
+        },
+        {
+          "name": "real_price",
+          "type": "text"
+        },
+        {
+          "name": "rating",
+          "type": "number"
+        },
+        {
+          "name": "rating_count",
+          "type": "number"
+        },
+        {
+          "name": "description",
+          "type": "text"
+        },
+        {
+          "name": "about",
+          "type": "text"
+        },
+        {
+          "name": "product_code",
+          "type": "text"
+        },
+        {
+          "name": "product_materials",
+          "type": "text"
+        }
+      ],
+      "outputType": "array"
     },
     "repeat3": {
       "meta": [
@@ -254,6 +380,77 @@ dmx.config({
     "varreview": {
       "meta": null,
       "outputType": "number"
+    },
+    "query": [
+      {
+        "type": "number",
+        "name": "product_id"
+      }
+    ],
+    "ddProductInformation": {
+      "meta": [
+        {
+          "name": "product_id",
+          "type": "number"
+        },
+        {
+          "name": "item_id",
+          "type": "number"
+        },
+        {
+          "name": "image",
+          "type": "text"
+        },
+        {
+          "name": "discount",
+          "type": "text"
+        },
+        {
+          "name": "type",
+          "type": "text"
+        },
+        {
+          "name": "name",
+          "type": "text"
+        },
+        {
+          "name": "color",
+          "type": "text"
+        },
+        {
+          "name": "final_price",
+          "type": "text"
+        },
+        {
+          "name": "real_price",
+          "type": "text"
+        },
+        {
+          "name": "rating",
+          "type": "number"
+        },
+        {
+          "name": "rating_count",
+          "type": "number"
+        },
+        {
+          "name": "description",
+          "type": "text"
+        },
+        {
+          "name": "about",
+          "type": "text"
+        },
+        {
+          "name": "product_code",
+          "type": "text"
+        },
+        {
+          "name": "product_materials",
+          "type": "text"
+        }
+      ],
+      "outputType": "array"
     }
   }
 });
